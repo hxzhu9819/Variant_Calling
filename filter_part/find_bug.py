68644f819cea88c33ade9c86d7e601aa15ccea5b
leevius <leevius@umich.edu>
2019-05-17 10:21:30 -0400

    come more test

---
+++ filter_part/find_bug.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+filename_original = '/Users/levius/Desktop/research_genomic_variant_call/my_method/Variant_Calling/filter_part/test1'
+filename_prune = '/Users/levius/Desktop/research_genomic_variant_call/my_method/Variant_Calling/filter_part/test2'
+
+file_origin = open(filename_original, 'r')
+file_prune = open(filename_prune, 'r')
+
+
